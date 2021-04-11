2f49ff0268d3265504e5b37fc0c4db8da6c8f146
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2021-04-11 08:03:47 -0700

    added some options

--- app.go
+++ app.go
@@ -20,11 +20,14 @@ func main(){
 }
 
 func game() {
-	var active bool= true
+	var active bool = true
+	var hint bool = false
+	var guessesLeft int = 8
+	var sguessesleft = strconv.Itoa(guessesLeft)
 	for active {
-		var correctAnswer int = rand.Intn(10)
+		var correctAnswer int = rand.Intn(20)
 		var scorrectAnswer = strconv.Itoa(correctAnswer)
-		fmt.Println("guess a number between 1 and 10")
+		fmt.Println("guess a number between 1 and 20")
 		var guess int
 		fmt.Scanln(&guess)
 		var sguess = strconv.Itoa(guess)
@@ -32,7 +35,30 @@ func game() {
 			fmt.Println("you got it! " + scorrectAnswer + " was the right number!")
 			break;
 		} else {
-			fmt.Println("sorry, " + sguess + " is not the right number!")
+			if hint == true {
+				if guess < correctAnswer{
+					fmt.Println("that's not it. the number is higher. guess again.")
+				}
+				if guess > correctAnswer {
+					fmt.Println("that's not it. the number is lower. guess aagin")
+				}
+			}
+			if  guessesLeft == 0 {
+				fmt.Println("sorry, you didn't guess the number in time. you've lost.")
+				break;
+			}
+			if  guessesLeft <= 3 && hint == false{
+				fmt.Println("you're alomost out of turns. would you like a hint?")
+				var activateHints string
+				fmt.Scanln(&activateHints)
+				if activateHints == "yes"{
+					hint = true
+				} else {
+					fmt.Println("okay, good luck")
+				}
+			}
+			guessesLeft -= 1
+			fmt.Println("sorry, " + sguess + " is not the right number! You have " + sguessesleft + " guesses left")
 		}
 	}
 }
